d05fc825257e39de607fff1d711cabd993b0a90c
JeongMin Choi <tlsdnjfwjdals159@gmail.com>
2022-05-08 15:04:09 +0900

    [UI] #5 : 설정 페이지 UI 완, 통계 그래프 설정 중

--- app/src/main/java/com/example/croffleproject/SettingPageActivity.java
+++ app/src/main/java/com/example/croffleproject/SettingPageActivity.java
@@ -1,14 +1,14 @@
 package com.example.croffleproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting_page);
     }
 }
